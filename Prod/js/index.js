d40baaa7d612c8618365e4c46ddd45f6bfc39694
chrisclayhendrik <chrisclayhendrik@gmail.com>
2021-09-15 02:32:29 -0500

    Completed Tasks 4 and 5, currently testing

--- Prod/js/index.js
+++ Prod/js/index.js
@@ -1,34 +1,36 @@
-
-const taskManager = new TaskManager();
-const inputForm = document.getElementById("#inputForm");
-
+let taskManager = new TaskManager();
 
 function validFormFieldInput(data) {
-    const newTaskNameInput = document.querySelector('#newTaskNameInput');
+
+    const newTaskNameInput = document.querySelector("#newTaskNameInput");
     const name = newTaskNameInput.value;
-    console.log("name: " + name);
+    console.log('name: ' + name);
 
-    const newTaskDescriptionInput = document.querySelector('#newTaskDescriptionInput');
+    const newTaskDescriptionInput = document.querySelector("#newTaskDescriptionInput");
     const description = newTaskDescriptionInput.value;
-    console.log("description: " + description);
+    console.log('description: ' + description);
 
-    const newAssignedToInput = document.querySelector('#newAssignedToInput');
+    const newAssignedToInput = document.querySelector("#newAssignedToInput");
     const assignedTo = newAssignedToInput.value;
-    console.log("assignedTo: " + assignedTo);
+    console.log('assignedTo: ' + assignedTo);
 
-    const newDueDateInput = document.querySelector('#newDueDateInput');
+    const newDueDateInput = document.querySelector("#newDueDateInput");
     const dueDate = newDueDateInput.value;
-    console.log("dueDate: " + dueDate);
+    console.log('dueDate: ' + dueDate);
 
-    const newStatusInput = document.querySelector('#newStatusInput');
-    const status = newStatusInput.value;
-    console.log("status: " + status);
+    const newStatus = document.querySelector("#newStatusInput");
+    const status = newStatus.value;
+    console.log('status: ' + status);
+   
+    taskManager.addTask(name, description, assignedTo, dueDate, status);
+    
+
 };
 
-
-document.getElementById("#newTaskButton").addEventListener("click", function() {
-    inputForm.submit();
-    taskManager.addTask(currentId, name, description, assignedTo, dueDate, status);
-}); 
-
+document.querySelector('#inputForm').addEventListener("submit", (event) => {
+    event.preventDefault();
+    validFormFieldInput();
+    document.querySelector('#inputForm').reset();
+      
+});
 console.log(taskManager.tasks);
