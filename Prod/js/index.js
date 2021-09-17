d757c618cad56bad9fc1432d6d9c798e873c4c7a
madhuanam <madhuanam@gmail.com>
2021-09-16 22:37:50 -0500

    added javascript

--- Prod/js/index.js
+++ Prod/js/index.js
@@ -1,3 +1,4 @@
+/*
 let taskManager = new TaskManager();
 
 function validFormFieldInput(data) {
@@ -34,3 +35,151 @@ document.querySelector('#inputForm').addEventListener("submit", (event) => {
 
 });
 console.log(taskManager.tasks);
+*/
+let initializePage = true ;
+const tm = new TaskManager();
+console.log(tm.tasks);
+console.log(tm.currentId);
+//tm.addTask('Take out the Trash','Take out the trash in front of the house','Nick','2020-01-12');
+//tm.addTask('Cook Dinner','Prepare a healthy serving of pancakes for the family tonight','Nick','2020-09-20');
+
+//Alerts - For error messages
+let taskNameAlert = document.getElementById('taskNameAlert');
+let taskDescAlert = document.getElementById('taskDescAlert');
+let assignedToAlert = document.getElementById('assignedToAlert');
+let dueDateAlert = document.getElementById('dueDateAlert');
+let statusAlert = document.getElementById('statusAlert');
+
+//Input fields for Tasks
+let taskNameInput = document.getElementById('taskName');
+let taskDescInput = document.getElementById('taskDesc');
+let assignedToInput = document.getElementById('assignedTo');
+let dueDateInput = document.getElementById('dueDate');
+let statusInput = document.getElementById('status');
+
+
+let taskNameVal;
+let taskDescVal;
+let assignedToVal;
+let dueDateVal;
+let statusVal;
+
+function validFormFieldInput(){
+	let allDataValid = true;
+	//alert('validFormFieldInput');
+	taskNameVal = taskNameInput.value;
+	//alert('Value of taskNameVal is ' + taskNameVal);
+	if(taskNameVal === ''){
+		//alert('Task name is null');
+		taskNameAlert.style.display = 'block';
+		allDataValid = false;
+	}
+	else{
+		taskNameAlert.style.display = 'none';
+	}
+	taskDescVal = taskDescInput.value;
+	if(taskDescVal === ''){
+		//alert('Task Desc is null');
+		taskDescAlert.style.display = 'block';
+		allDataValid = false;
+	}
+	else{
+		taskDescAlert.style.display = 'none';
+	}
+	assignedToVal = assignedToInput.value;
+	if(assignedToVal === ''){
+		//alert('Assigned To is null');
+		assignedToAlert.style.display = 'block';
+		allDataValid = false;
+	}
+	else{
+		assignedToAlert.style.display = 'none';
+	}
+	dueDateVal = dueDateInput.value;
+	if(dueDateVal === ''){
+		//alert('Due Date is null');
+		dueDateAlert.style.display = 'block';
+		allDataValid = false;
+	}
+	else{
+		dueDateAlert.style.display = 'none';
+	}
+	statusVal = statusInput.value;
+	if(statusVal === ''){
+		//alert('Due Date is null');
+		statusAlert.style.display = 'block';
+		allDataValid = false;
+	}
+	else{
+		statusAlert.style.display = 'none';
+	}
+	return allDataValid;
+};
+
+function clearFields() {
+	taskNameInput.value='';
+	taskDescInput.value='';
+	assignedToInput.value='';
+	dueDateInput.value='';
+	statusInput.value='';
+}
+function addTaskInformation() {
+	/*
+        const newTaskNameInput = document.querySelector('#taskName');
+        const name = newTaskNameInput.value;
+	console.log("name : " + name);
+	*/
+
+        allDataValid = validFormFieldInput();
+	//alert(allDataValid);
+	if(allDataValid){
+		//Clear the field values
+		clearFields();
+		//Add the field values to task
+		tm.addTask(taskNameVal,taskDescVal,assignedToVal,dueDateVal,statusVal);
+		console.log(tm.tasks);
+		console.log(tm.currentId);
+
+		tm.render();
+	}
+}
+
+function hideAlert(){
+	//alert('in hideAlert');
+	taskNameAlert.style.display = 'none';
+	taskDescAlert.style.display = 'none';
+	assignedToAlert.style.display = 'none';
+	dueDateAlert.style.display = 'none';
+	statusAlert.style.display = 'none';
+}
+
+function initialize(){
+	if(initializePage){
+		hideAlert();
+		initializePage = false;
+	}
+}
+
+//addTaskBtn.onclick = addTaskInformation;
+addTaskBtn.addEventListener('click',addTaskInformation);
+
+
+//code to initialize the page
+initialize();
+
+//taskHtml = createTaskHtml('TaskTest','Descr','Nick1','10/20/2020','TODO');
+//console.log(taskHtml);
+let taskListGroupElem = document.getElementById('taskListGroupId');
+taskListGroupElem.addEventListener('click',(event)=> {
+	if(event.target.id === "markDoneBtn"){
+		//alert(event.target.id);
+		//alert(event.target.parentElement.id);
+		let foundTask = tm.getTaskById(event.target.parentElement.id);
+		//alert(foundTask);
+		//Update the status to DONE
+                foundTask.status = 'Done';
+
+		//Rerender the page
+		tm.render();
+	}	
+});
--- Prod/js/taskManager.js
+++ Prod/js/taskManager.js
@@ -1,37 +1,44 @@
-const createTaskHtml = (name, description, assignedTo, dueDate, status) => {
+/*
+const createTaskHtml = (id,name, description, assignedTo, dueDate, status) => {
     const html = `<li class="list-group-item">
-    <div class="card">
-        <div class="card-body">
-            <h5 class="card-title">${name}</h5>
-            <p class="card-text">${description}</p>
-        </div>
-        <ul class="list-group list-group-flush">
-            <li class="list-group-item">${assignedTo}</li>
-            <li class="list-group-item">${dueDate}</li>
-            <li class="list-group-item">
-                <div class="dropdown">
-                    <button class="btn btn-secondary dropdown-toggle" type="button"
-                        id="dropdownMenu2" data-bs-toggle="dropdown" aria-expanded="false">
-                        ${status}
-                    </button>
-                    <ul class="dropdown-menu" aria-labelledby="dropdownMenu2">
-                        <li><button class="btn btn-success dropdown-item"
-                                type="button">Action</button></li>
-                        <li><button class="dropdown-item" type="button">Another action</button></li>
-                        <li><button class="dropdown-item" type="button">Something else here</button>
-                        </li>
-                    </ul>
-                </div>
-            </li>
-        </ul>
-    </div>
-</li>`
+                        <div class="card" id="${id}">
+                            <div class="card-body">
+                                <h5 class="card-title">${name}</h5>
+                                <li class="list-group-item">${description}</li>
+                                <li class="list-group-item">${assignedTo}</li>
+                                <li class="list-group-item">${dueDate}</li>
+				<li class="list-group-item">${status}</li>
+				<input type="button" id="markDoneBtn" class="done-button" value="Mark Done">				
+                            </div>
+                        </div>
+                    </li>`
     return html;
 }
+*/
+
+const createTaskHtml = (id,name,desc,assignedTo,dueDate,status) => {
+	const html = `
+		      <li class="list-group-item">	
+              <div class="col pl-40">
+                        <div class="card border-primary">
+                                <div class="card-body" id="${id}">
+                                        <h4 class="card-title">${name}</h4>
+                                        <p class="card-text">${desc}</p>
+                                        <p class="card-text">${assignedTo}</p>
+                                        <p class="card-text">${dueDate}</p>
+                                        <p class="card-text">${status}</p>
+				        <input type="button" id="markDoneBtn" class="done-button" value="Mark Done">
+                                </div>
+                        </div>      
+              </div> 
+	      </li>`;
+	return html;
+
+};
 
 
 
-
+/*
 class TaskManager {
     constructor(currentId = 0) {
         this.tasks = [];
@@ -42,3 +49,67 @@ class TaskManager {
         this.tasks.push(this.currentId, name, description, assignedTo, dueDate, status);
     };
 };
+*/
+class TaskManager {
+	constructor(){
+  	this._currentId = 0;
+		this._tasks = [];
+	}
+  get currentId(){
+  	return this._currentId;
+  }
+  set currentId(cid){
+  	this._currentId = cid;
+  }
+  get tasks(){
+  	return this._tasks;
+  }
+  addTask(name,description,assignedTo,dueDate,status){
+  	const cid = this.currentId;
+  	let task = {
+    	id:cid,
+      name:name,
+      description:description,
+      assignedTo:assignedTo,
+      dueDate:dueDate,
+      status:status //'TODO'
+	};
+    //let task = [10];
+    this._tasks.push(task);
+    this.currentId += 1;
+  }
+
+  render(){
+	  let tasksHTMLList = [];
+	  let taskHTML ;
+	  this._tasks.forEach(function(task) {
+		//date = new Date(task[dueDate]);
+		//formattedDate = 
+		taskHTML = createTaskHtml(task.id,task.name,task.description,task.assignedTo,task.dueDate,task.status);
+		//alert(taskHTML);
+		tasksHTMLList.push(taskHTML);
+	  });
+
+	  let taskHtmlStr = tasksHTMLList.join('\n');
+
+	  let taskListGroupElem = document.getElementById('taskListGroupId');
+	  taskListGroupElem.innerHTML = taskHtmlStr;
+  }
+
+  getTaskById(taskId){
+	  let foundTask ;
+	  //alert('In class ' + taskId);
+	  this._tasks.forEach(function(task) {
+	          //alert('Task Id ' + task.id);
+		  if(task.id == taskId){
+	                  //alert('Matched');
+			  foundTask = task;
+	                  //alert('Found task' + foundTask.id);
+			  //break;
+		  }
+	  });
+	  return foundTask;
+  }
+
+} //end class TaskManager
+
