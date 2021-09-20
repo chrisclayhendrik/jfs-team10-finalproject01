895881ae484c100bcfccf25d27c4e581d06b0ff5
chrisclayhendrik <chrisclayhendrik@gmail.com>
2021-09-20 15:31:18 -0500

    Setup mocha

---
+++ test/test.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+const TaskManager = require('../Prod/js/taskManager.js');
+
+describe('TaskManager', () => {
+    describe('.addTask', () => {
+        it('pushes form inputs to the tasks array and creates a task object', () => {
+
+        });
+    });
+    describe('.deleteTask', () => {
+        it('deletes the task with a specific id from the tasks array and deletes the task object', () => {
+
+        });
+    });
+    describe('getTaskById', () => {
+        it('returns a task object with the given id', () => {
+
+        });
+    });
+});
